b02cb92747cc872b69ca60ea701c2e0cc067264f
Niujunbo2002 <21376334@buaa.edu.cn>
2026-03-22 23:37:33 +0800

    Add bbox utils module

--- mineru_diffusion/__init__.py
+++ mineru_diffusion/__init__.py
@@ -1,1 +1,3 @@
-__all__ = []
+from .utils import draw_bbox, parse_bbox_prompt
+
+__all__ = ["draw_bbox", "parse_bbox_prompt"]
---
+++ mineru_diffusion/utils/__init__.py
@@ -0,0 +1,3 @@
+from .bbox import draw_bbox, parse_bbox_prompt
+
+__all__ = ["draw_bbox", "parse_bbox_prompt"]
